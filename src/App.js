1cc35d2ab8aa078bbc98393bc45bf7a450d2d476
Akhil Sahota <akhil.sahota@kuliza.com>
2020-05-27 12:52:08 +0530

    Header component | links to home, shop and contact page

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import './App.css';
 
 import { HomePage } from './containers/Homepage/HomepageComponent';
 import ShopPage from './containers/Shop/ShopComponent';
+import Header from './components/Header/HeaderComponent';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={HomePage} /> 
-      <Route exact path='/shop' component={ShopPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} /> 
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
