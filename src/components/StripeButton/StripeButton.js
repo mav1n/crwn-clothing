5f4cdaa64bb7d51bc8abef8fdf440f97f70dc013
Akhil Sahota <akhil.sahota@kuliza.com>
2020-08-10 22:11:56 +0530

    updating stripe integration and title

---
+++ src/components/StripeButton/StripeButton.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const publishableKey =
+    'pk_test_51HEdJYJmkeWgO5SvaaIOFSgeKlk35pcPPbDX8YzSs0gS5NaSupCmaXBX2Rk7J06yfSdarFfi2bMm0dusD4lD5FPn00vHM1JnNy';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is ${price}`}
+      amount={price}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
