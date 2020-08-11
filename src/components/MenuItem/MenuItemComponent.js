bb84a00f2c20d9a0e91b6398cd5de179949908fa
Akhil Sahota <akhil.sahota@kuliza.com>
2020-08-11 15:09:21 +0530

    favicon changed | link from homage to collection items changed

--- src/components/MenuItem/MenuItemComponent.js
+++ src/components/MenuItem/MenuItemComponent.js
@@ -3,15 +3,24 @@ import './MenuItem.styles.scss';
 import { withRouter } from 'react-router-dom';
 
 const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => (
-    <div className={`${size} menu-item`} onClick={() => history.push(`${match.path}${linkUrl}`)}>
-        <div className='background-image' style={{
-            backgroundImage: `url(${imageUrl})`
-        }} />
-        <div className='content'>
-            <h1 className='title'>{title.toUpperCase()}</h1>
-            <span className='subtitle'>SHOP NOW</span>
-        </div>
+  <div
+    className={`${size} menu-item`}
+    onClick={() => {
+      console.log(match.path, linkUrl);
+      return history.push(`${match.path}shop/${linkUrl}`);
+    }}
+  >
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">SHOP NOW</span>
     </div>
-)
+  </div>
+);
 
 export default withRouter(MenuItem);
